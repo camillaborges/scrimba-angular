6fe3c9a1d221666248b9d7a210a08fb2e4f2ad96
Borges Camilla <camillaborgeshelena@gmail.com>
2022-01-05 16:46:04 +0100

    linking to routes with the routerLink directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { SharedModule } from './shared/shared.module';
 import { CustomersModule } from './customers.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    OrdersComponent,
   ],
 
   imports: [
--- src/app/customers.module.ts
+++ src/app/customers.module.ts
@@ -6,11 +6,12 @@ import { SharedModule } from './shared/shared.module';
 import { CustomersComponent } from './component/customers/customers.component';
 import { CustomersListComponent } from './customers/customers-list/customers-list.component';
 import { FilterTextBoxComponent } from './customers/customers-list/filter-textbox.component';
+import { CustomersRoutingModule } from './component/customers/customers-routing.module';
 
 @NgModule({
-    imports: [ CommonModule, SharedModule, FormsModule ],
+    imports: [ CommonModule, SharedModule, FormsModule, CustomersRoutingModule ],
     declarations: [ CustomersComponent, CustomersListComponent, FilterTextBoxComponent ],
     exports: [ CustomersComponent ]
 })
 
-export class CustomersModule { }
+export class CustomersModule { } 
